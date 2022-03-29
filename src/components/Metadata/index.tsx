db2f552f113df0791dcff6107b0e3c04897e3292
Bruno <brunoyoichi@hotmail.com>
2022-03-29 16:23:06 -0400

    feat(metadata component): add twitter and facebook open graph tags

--- src/components/Metadata/index.tsx
+++ src/components/Metadata/index.tsx
@@ -4,13 +4,25 @@ import { useTranslation } from 'next-i18next'
 
 function Metadata() {
   const { t } = useTranslation('metadata')
+
   return (
     <Head>
       <title>{t('title')}</title>
       <meta name="description" content={t('description')} key="desc" />
       <meta property="og:title" content={t('title')} />
       <meta property="og:description" content={t('ogDescription')} />
-      <meta property="og:image" content="/nextjs-logo.svg" />
+      <meta property="og:type" content="website" />
+      <meta property="twitter:card" content="summary" />
+      <meta property="twitter:title" content={t('title')} />
+      <meta property="twitter:description" content={t('ogDescription')} />
+      <meta
+        property="twitter:image"
+        content="https://avatars.githubusercontent.com/u/37604496?v=4"
+      />
+      <meta
+        property="og:image"
+        content="https://avatars.githubusercontent.com/u/37604496?v=4"
+      />
     </Head>
   )
 }
