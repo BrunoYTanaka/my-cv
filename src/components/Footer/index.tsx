cd816f7cb4ea2ab8a13572bfad7d19f4c13a595b
Bruno Y. Tanaka <bruno.tanaka@agilecontent.com>
2022-10-25 15:24:48 -0400

    feat(footer): add thank you message to footer
    Add thank you for coming message and remove fade in animation from footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -2,20 +2,17 @@ import { ReactElement } from 'react'
 import { List, ListItemButton } from '@/components/List'
 import { layout } from '@/config'
 import { icons } from '@/constants/icons'
-import { defaultTransition, fadeInUpVariants } from '@/constants/framerMotion'
 import * as S from './styles'
+import { Typography } from '../Typography'
+import FavoriteIcon from '@mui/icons-material/Favorite'
+import { useTranslation } from 'next-i18next'
 
 function Footer(): ReactElement {
+  const { t } = useTranslation('footer')
   const { socialMedia } = layout.contact
 
   return (
-    <S.Footer
-      initial="hidden"
-      whileInView="visible"
-      variants={fadeInUpVariants}
-      viewport={{ once: true }}
-      transition={defaultTransition}
-    >
+    <S.Footer>
       <List direction="row" gap={20} mt={0} mb={0}>
         {socialMedia.map((media) => {
           const Icon = icons[media.icon]
@@ -28,6 +25,12 @@ function Footer(): ReactElement {
           )
         })}
       </List>
+      <S.FooterMessage>
+        <Typography useAnimation={false} variant="subtitle1">
+          {t('thank-you-for-coming')}
+        </Typography>
+        <FavoriteIcon color="error" />
+      </S.FooterMessage>
     </S.Footer>
   )
 }
--- src/components/Footer/styles.ts
+++ src/components/Footer/styles.ts
@@ -1,11 +1,16 @@
 import { styled } from '@mui/material/styles'
-import { motion } from 'framer-motion'
 
-export const Footer = styled(motion.footer)(({ theme }) => ({
+export const Footer = styled('footer')(({ theme }) => ({
   display: 'flex',
   alignItems: 'center',
   justifyContent: 'space-evenly',
   width: '100%',
   height: '5rem', //80px
   backgroundColor: theme.palette.neutral,
+}))
+
+export const FooterMessage = styled('div')(({ theme }) => ({
+  display: 'flex',
+  alignItems: 'flex-start',
+  gap: '0.5rem',
 }))
